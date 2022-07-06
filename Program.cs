37e49483724625a3211e7d7aba389b699e872e5e
caiomaestrello <105826879+caiomaestrello@users.noreply.github.com>
2022-07-06 18:35:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avaliação5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, a;
+
+            for (i = 1; i <= 100; i++)
+            {
+                a = i % 2;
+                if (a == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
